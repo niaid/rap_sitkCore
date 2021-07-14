096829e0b55c87eb3e6e7c40573f97d114c1519b
Bradley Lowekamp <blowekamp@mail.nih.gov>
2021-07-14 15:25:04 -0400

    teach read_dcm to convert color images to grayscale
    Implement handling of different photometric types for pydicom, and
    convert to RGB. Use srgbtogray to convert RGB images to grayscale.

--- rap_sitkcore/read_dcm.py
+++ rap_sitkcore/read_dcm.py
@@ -1,6 +1,7 @@
 import SimpleITK as sitk
 import pydicom
 from pathlib import Path
+from rap_sitkcore._util import srgb2gray
 
 
 def _read_dcm_pydicom(filename: Path) -> sitk.Image:
@@ -9,11 +10,32 @@ def _read_dcm_pydicom(filename: Path) -> sitk.Image:
     """
     ds = pydicom.dcmread(filename)
     img = sitk.GetImageFromArray(ds.pixel_array, isVector=(len(ds.pixel_array.shape) == 3))
-    if img.GetNumberOfComponentsPerPixel() != 1:
-        img = sitk.VectorMagnitude(img)
+
+    arr = ds.pixel_array
+
+    if ds.PhotometricInterpretation == "MONOCHROME2":
+        img = sitk.GetImageFromArray(arr, isVector=False)
+
+    elif ds.PhotometricInterpretation == "MONOCHROME1":
+        # only works with unsigned
+        assert ds.PixelRepresentation == 0
+        # use complement to invert the pixel intensity.
+        img = sitk.GetImageFromArray(~arr, isVector=False)
+    elif ds.PhotometricInterpretation in ["YBR_FULL_422", "YBR_FULL", "RGB"]:
+        from pydicom.pixel_data_handlers.util import convert_color_space
+
+        if ds.PhotometricInterpretation != "RGB":
+            arr = convert_color_space(ds.pixel_array, ds.PhotometricInterpretation, "RGB")
+
+        img = sitk.GetImageFromArray(arr, isVector=True)
+    else:
+        raise RuntimeError(f'Unsupported PhotometricInterpretation: "{ds.PhotometricInterpretation}"')
+
     if "Modality" in ds:
         de = ds.data_element("Modality")
         img.SetMetaData(f"{de.tag.group:04x}|{de.tag.elem:04x}", de.value)
+    # TODO Set spacing and origin etc...
+
     return img
 
 
@@ -32,6 +54,7 @@ def _read_dcm_sitk(filename: Path) -> sitk.Image:
         image_size[2] = 0
         image_file_reader.SetExtractSize(image_size)
 
+    image_file_reader.GetNumberOfComponents()
     return image_file_reader.Execute()
 
 
@@ -39,6 +62,7 @@ def read_dcm(filename: Path) -> sitk.Image:
     """
     Read an x-ray DICOM file with GDCMImageIO, reducing it to 2D from 3D as needed.
     If the file cannot be read by the GDCM library, then pydicom is tried.
+    Color images are converted to grayscale.
     :param filename: A DICOM filename
     :return: a 2D SimpleITK Image
     """
@@ -47,10 +71,16 @@ def read_dcm(filename: Path) -> sitk.Image:
         raise IOError(f'The file: "{filename}" does not exist.')
 
     try:
-        return _read_dcm_sitk(filename)
+        img = _read_dcm_sitk(filename)
     except RuntimeError as e:
         try:
-            return _read_dcm_pydicom(filename)
+            img = _read_dcm_pydicom(filename)
         except Exception:
-            # Reraise exception from SimpleITK's GDCM reading
+            # Re-raise exception from SimpleITK's GDCM reading
             raise e
+
+    if img.GetNumberOfComponentsPerPixel() == 1:
+        return img
+    elif img.GetNumberOfComponentsPerPixel() == 3:
+        return srgb2gray(img)
+    raise RuntimeError(f"Unsupported number of components: {img.GetNumberOfComponentsPerPixel()}")
